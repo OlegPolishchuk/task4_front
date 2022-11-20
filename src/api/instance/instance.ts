59703c09fe76b7b6da0c1b5a2880bb25f7eddfea
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-11-20 15:12:29 +0300

    fix: axios instance base_url

--- src/api/instance/instance.ts
+++ src/api/instance/instance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import {AuthEndpoints} from "enums";
 
 export const instance = axios.create({
-  baseURL: 'http://localhost:5000/'
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/'
 })
 
 const skipUrls = [AuthEndpoints.Logout, AuthEndpoints.Login, AuthEndpoints.Refresh] as string[];
